f1cb8fffa49b90cb0effe1bb5ff2b92250a607cc
kokokoroDev <youneslahlibi@gmail.com>
2025-11-06 13:36:36 +0100

    fixed env importation

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,10 +1,17 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-export const supabase = createClient<Database>(import.meta.env.VITE_SUPABASE_URL!, process.env.VITE_SUPABASE_PUBLISHABLE_KEY!, {
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL or Key is missing!");
+}
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
   auth: {
     storage: localStorage,
     persistSession: true,
     autoRefreshToken: true,
-  }
+  },
 });
